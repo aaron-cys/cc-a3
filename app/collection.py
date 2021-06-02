c1a160ade3fc6dc4a3fa2786f3187973ae3ce25b
amyyynn <s3783694@student.rmit.edu.au>
2021-06-02 21:38:04 +1000

    Add individual product page

--- app/collection.py
+++ app/collection.py
@@ -13,34 +13,63 @@ collection = Blueprint('collection', __name__)
 aws_access_key_id = "AKIAVQHEZJ6A4MBK2V5K"
 aws_secret_access_key = "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"
 
+# Index 
+@collection.route('/')
+def index():
+    product_list = get_products_by_category()
+    new = get_new()
+
+    return render_template('index.html', product_list=product_list, new=new)
+
 # Collection category page
-@collection.route("/collection")
+@collection.route('/collection')
 def collectionList():
     error = None
-    product_list = get_products()
+    product_list = get_products_by_category()
 
     return render_template('collection.html', error=error, product_list=product_list)
 
 # Women category page
-@collection.route("/women")
+@collection.route('/women')
 def women():
     product_list = get_women()
     return render_template('women.html', product_list=product_list)
 
 # Men category page
-@collection.route("/men")
+@collection.route('/men')
 def men():
     product_list = get_men()
     return render_template('men.html', product_list=product_list)
 
 # New category page
-@collection.route("/new")
+@collection.route('/new')
 def new():
     product_list = get_new()
     return render_template('new.html', product_list=product_list)
 
-# Scan and get products from 'product' table
-def get_products(dynamodb=None):
+# Individual product page
+@collection.route('/product/<product_name>', methods=['GET', 'POST'])
+def product(product_name):
+    product_list = get_products_by_name(product_name)
+    return render_template('product.html', product_name=product_name, product_list=product_list)
+
+
+
+
+# Scan and get products from 'product' table by NAME
+def get_products_by_name(name, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2',
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    table = dynamodb.Table('product')
+    response = table.query(
+        KeyConditionExpression=Key('name').eq(name)
+    )
+    return response['Items']
+
+# Scan and get products from 'product' table by CATEGORY
+def get_products_by_category(dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2',
         aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
--- main.py
+++ main.py
@@ -13,19 +13,8 @@ from app.collection import collection
 app = Flask(__name__)
 app.secret_key = "secretKey000"
 
-aws_access_key_id = "AKIAVQHEZJ6A4MBK2V5K"
-aws_secret_access_key = "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"
-
 # Blueprints
 app.register_blueprint(collection)
-
-# Index 
-@app.route('/')
-def index():
-    product_list = get_products()
-    new = get_new()
-
-    return render_template('index.html', product_list=product_list, new=new)
 
 # Login
 @app.route('/login', methods=['GET', 'POST'])
@@ -40,30 +29,6 @@ def signup():
     error = None
 
     return render_template('signup.html', error=error)
-
-# Scan and get products from 'product' table
-def get_products(dynamodb=None):
-    if not dynamodb:
-        dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2',
-        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-
-    table = dynamodb.Table('product')
-    response = table.scan(
-        FilterExpression=Attr('category').eq("Women") | Attr('category').eq("Men") | Attr('category').eq("New")
-    )
-    return response['Items']
-
-# Scan and get 'New' products from 'product' table
-def get_new(dynamodb=None):
-    if not dynamodb:
-        dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2',
-        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-
-    table = dynamodb.Table('product')
-    response = table.scan(
-        FilterExpression=Attr('category').eq("New")
-    )
-    return response['Items']
 
 # Run App
 if __name__ == "__main__":
