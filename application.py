b9caf897a1b89a386538252a8178e3b5c0bf4664
Aaron <s3786067@student.rmit.edu.au>
2021-06-13 14:17:34 +1000

    Merge with main & Update Google acc payment

--- application.py
+++ application.py
@@ -37,7 +37,7 @@ google_client_secret = os.path.join(Path(__file__).parent, "static/arika_google.
 flow = Flow.from_client_secrets_file(
     client_secrets_file=google_client_secret,
     scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="http://arika-env.eba-f596cma3.ap-southeast-2.elasticbeanstalk.com/callback"
+    redirect_uri="http://127.0.0.1:5000/callback"
                                      )
 
 # Stripe Keys
@@ -123,7 +123,6 @@ def login():
         
         return render_template('login.html')
 
-
 # Google call back
 @application.route("/callback")
 def gcallback():
@@ -166,7 +165,7 @@ def signup():
             pnumber = request.form["pnumber"]
             address = request.form["address"]
             password = request.form["password"]
-            logged = signedUp(username, password, gname, fname, email, pnumber, address, valid, error)
+            logged = signedUp(username, password, gname, fname, email, pnumber, address, valid)
             if(logged[0:] == "true"):
                 return redirect(url_for("confirm"))
             else:
@@ -178,7 +177,6 @@ def signup():
 
 
         return render_template('signup.html', error=error)
-
 
 # Confirm
 @application.route('/confirmation', methods=['GET', 'POST'])
@@ -205,7 +203,6 @@ def confirm():
 
         return render_template('confirm.html', error=error)
 
-
 # Profile
 @application.route('/profile')
 def profile():
@@ -215,6 +212,20 @@ def profile():
     
     return render_template('profile.html', u_session=u_session, userInfo=userInfo)
 
+# Size Measurement Tool
+@application.route('/size_tool', methods=['GET', 'POST'])
+def size_tool():
+    u_session = check_user_session()
+    size = None
+
+    if request.method == "POST":
+        bust = int(request.form["bust"])
+        waist = int(request.form["waist"])
+        hip = int(request.form["hip"])
+        size = calculate_size(bust, waist, hip)
+        return render_template('size_tool.html', u_session=u_session, size=size)
+    else:
+        return render_template('size_tool.html', u_session=u_session)
 
 # Collection category page
 @application.route('/collection')
@@ -252,18 +263,26 @@ def product(product_name):
     u_session = check_user_session()
     message = None
     product_list = get_products_by_name(product_name)
+    reviews = get_reviews(product_name)
 
     if request.method == 'POST':
-        if 'bag' not in session:
-            session['bag'] = []
-        bag = session['bag']
-        bag.append(request.form['product_name'])
-        session['bag'] = bag
-        message = "Successfully added to bag!"
-        return render_template('product.html', u_session=u_session, message=message, product_name=product_name, product_list=product_list, bag=bag)
-    else:
-        return render_template('product.html', u_session=u_session, message=message, product_name=product_name, product_list=product_list)
+        # If user is logged in, then allow them to add product to bag
+        if u_session:
+            # Store product in bag session
+            if 'bag' not in session:
+                session['bag'] = []
+            bag = session['bag']
+            bag.append(request.form['product_name'])
+            bag.append(request.form.get('size'))
+            session['bag'] = bag
 
+            message = "Successfully added to bag!"
+            return render_template('product.html', reviews=reviews, u_session=u_session, message=message, product_name=product_name, product_list=product_list, bag=bag)
+        else:
+            message = "Please log in to add to bag"
+            return render_template('product.html', reviews=reviews, u_session=u_session, message=message, product_name=product_name, product_list=product_list)
+    else:
+        return render_template('product.html', reviews=reviews, u_session=u_session, message=message, product_name=product_name, product_list=product_list)
 
 # Bag/cart page
 @application.route('/bag', methods=['GET', 'POST'])
@@ -278,7 +297,13 @@ def bag():
         product_list = []
         for product in bag:
             p_list = product_list
-            p_list.append(get_products_by_name(product))
+            # Check if element in session is size (determined by uppercase) or product list
+            if product.isupper():
+                size = []
+                size.append({'size':product, 'price':0, 'name':'Dummy', 'popularity':0})
+                p_list.append(size)
+            else:
+                p_list.append(get_products_by_name(product))
             product_list = p_list
 
         for p in product_list:
@@ -286,6 +311,7 @@ def bag():
             session['total'] = total * 100
 
         if request.method == 'POST':
+            # Empty the bag from session if selected
             if 'remove' in request.form:
                 session.pop('bag', None)
                 return redirect(url_for('bag', u_session=u_session, error=error))
@@ -301,6 +327,7 @@ def create_stripe_session():
 
     total = session.get('total', None)
 
+    # Create Stripe session with details
     stripe_session = stripe.checkout.Session.create(
         payment_method_types=['card'],
         line_items=[{
@@ -314,29 +341,46 @@ def create_stripe_session():
         'quantity': 1,
         }],
         mode='payment',
-        success_url='http://127.0.0.1:8080/success_stripe',
-        cancel_url='http://127.0.0.1:8080/bag',
+        success_url='http://127.0.0.1:5000/success_stripe',
+        cancel_url='http://127.0.0.1:5000/bag',
     )
     return jsonify({"sessionId": stripe_session["id"]})
-
 
 # Success page for Stripe
 @application.route('/success_stripe')
 def success_stripe():
     u_session = check_user_session()
+    userInfo = getUser()
+    username = None
+
+    if userInfo[0] == "cognito":
+        username = userInfo[1]
+    elif userInfo[0] == "google":
+        username = userInfo[2].split("@", 1)[0]
+
     if 'bag' in session:
         bag = session['bag']
         product_list = []
         for product in bag:
             p_list = product_list
-            p_list.append(get_products_by_name(product))
+            if product.isupper():
+                size = []
+                size.append({'size':product, 'price':0, 'name':'Dummy', 'popularity':0})
+                p_list.append(size)
+            else:
+                p_list.append(get_products_by_name(product))
             product_list = p_list
         
         for p in product_list:
+            # Increment popularity by 1 for purchased products
             name = p[0]['name']
-            popularity = p[0]['popularity'] 
-            popularity += 1
-            update_popularity(name, popularity)
+            if name != "Dummy":
+                popularity = p[0]['popularity'] 
+                popularity += 1
+                update_popularity(name, popularity)
+
+                # Add order into 'order' history table
+                put_order(username, name)
 
     session.pop('bag', None)
     session.pop('total', None)
@@ -346,28 +390,85 @@ def success_stripe():
 @application.route('/success_paypal')
 def success_paypal():
     u_session = check_user_session()
+    userInfo = getUser()
+    username = None
+
+    if userInfo[0] == "cognito":
+        username = userInfo[1]
+    elif userInfo[0] == "google":
+        username = userInfo[2].split("@", 1)[0]
+
     if 'bag' in session:
         bag = session['bag']
         product_list = []
         for product in bag:
             p_list = product_list
-            p_list.append(get_products_by_name(product))
+            if product.isupper():
+                size = []
+                size.append({'size':product, 'price':0, 'name':'Dummy', 'popularity':0})
+                p_list.append(size)
+            else:
+                p_list.append(get_products_by_name(product))
             product_list = p_list
         
         for p in product_list:
+            # Increment popularity by 1 for purchased products
             name = p[0]['name']
-            popularity = p[0]['popularity'] 
-            popularity += 1
-            update_popularity(name, popularity)
+            if name != "Dummy":
+                popularity = p[0]['popularity'] 
+                popularity += 1
+                update_popularity(name, popularity)
+
+                # Add order into 'order' history table
+                put_order(username, name)
 
     session.pop('bag', None)
     session.pop('total', None)
     return render_template('success_paypal.html', u_session=u_session)
 
+# Order history page
+@application.route('/order_history')
+def order_history():
+    u_session = check_user_session()
+    userInfo = getUser()
+    username = None
+
+    if userInfo[0] == "cognito":
+        username = userInfo[1]
+    elif userInfo[0] == "google":
+        username = userInfo[2].split("@", 1)[0]
+
+    order_list = get_orders(username)
+    product_list = []
+    for o in order_list:
+        p_list = product_list
+        p_list.append(get_products_by_name(o['product']))
+        product_list = p_list
+
+    return render_template('order_history.html', u_session=u_session, username=username, product_list=product_list)
+
+# Add review page
+@application.route('/add_review/<username>/<product>', methods=['GET', 'POST'])
+def review(username, product):
+    u_session = check_user_session()
+    success_msg = None
+
+    # Add and store review in database
+    if request.method == "POST":
+        subject = request.form["subject"]
+        message = request.form["message"]
+        rating = int(request.form["rating"])
+        put_review(username, product, subject, message, rating)
+        success_msg = "Review added successfully!"
+        return render_template('review.html', u_session=u_session, success_msg=success_msg, username=username, product=product)
+    else:
+        return render_template('review.html', u_session=u_session, success_msg=success_msg, username=username, product=product)
+
 
 # Working sign up =================================================================================
 def signedUp(username, password, gname, fname, email, pnumber, address, valid):
-    client = boto3.client('cognito-idp', region_name='ap-southeast-2')
+    client = boto3.client('cognito-idp', region_name='ap-southeast-2',
+         aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
     try:
         client.sign_up(
@@ -407,7 +508,8 @@ def signedUp(username, password, gname, fname, email, pnumber, address, valid):
 
 # confirmation =========================================================================================
 def userConfirm(username, code, valid, error):
-    client = boto3.client('cognito-idp', region_name='ap-southeast-2')
+    client = boto3.client('cognito-idp', region_name='ap-southeast-2',
+         aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
     try:
         client.confirm_sign_up(
@@ -424,7 +526,8 @@ def userConfirm(username, code, valid, error):
 
 # Check logged in =================================================================================
 def loggedIn(username, password, valid, error):
-    client = boto3.client('cognito-idp', region_name='ap-southeast-2')
+    client = boto3.client('cognito-idp', region_name='ap-southeast-2',
+         aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
     try:
         response = client.initiate_auth(
@@ -448,7 +551,8 @@ def loggedIn(username, password, valid, error):
 # Get user
 def getUser():
     if 'user' in session:
-        client = boto3.client('cognito-idp', region_name='ap-southeast-2')
+        client = boto3.client('cognito-idp', region_name='ap-southeast-2',
+         aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
         cognito_at = session['cognito_at']
 
         response = client.get_user(
@@ -483,7 +587,7 @@ def getUser():
         userData.append(email)
         
         return userData
-        
+
 
 # Products DB =====================================================================================
 
@@ -491,7 +595,7 @@ def getUser():
 def update_popularity(name, popularity, dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2',
-                                  aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+         aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
     table = dynamodb.Table('product')
     response = table.update_item(
@@ -508,8 +612,8 @@ def update_popularity(name, popularity, dynamodb=None):
 # Scan and get products from 'product' table by NAME
 def get_products_by_name(name, dynamodb=None):
     if not dynamodb:
-        dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2',
-                                  aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+        dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2', 
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
     table = dynamodb.Table('product')
     response = table.query(
@@ -520,8 +624,8 @@ def get_products_by_name(name, dynamodb=None):
 # Scan and get products from 'product' table by CATEGORY
 def get_products_by_category(dynamodb=None):
     if not dynamodb:
-        dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2',
-                                  aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+        dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2', 
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
     table = dynamodb.Table('product')
     response = table.scan(
@@ -566,14 +670,81 @@ def get_new(dynamodb=None):
     )
     return response['Items']
 
-def get_popular():
-    lambda_client = boto3.client('lambda', region_name='ap-southeast-2', 
-    aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+# Size Measurement Tool ============================================================================
 
-    result = lambda_client.invoke(FunctionName='get-popular-items-dynamodb', InvocationType='RequestResponse', Payload='{}')
+# Calculate size
+def calculate_size(bust, waist, hip):
+    if (bust < 83) & (waist < 68) & (hip < 91):
+        return 'XS'
+    elif (bust >= 83 & bust < 88) & (waist >= 68 & waist < 73) & (hip >= 91 & hip < 96):
+        return 'S'
+    elif (bust >= 88 & bust < 93) & (waist >= 73 & waist < 78) & (hip >= 96 & hip < 101):
+        return 'M'
+    elif (bust >= 93 & bust < 98) & (waist >= 78 & waist < 83) & (hip >= 101 & hip < 106):
+        return 'L'
+    elif (bust >= 98) & (waist >= 83) & (hip >= 106):
+        return 'XL'
 
-    items = result['Payload'].read()
-    return items
+
+# Order History + Review Feature ===================================================================
+
+# Get order history from 'order' table
+def get_orders(username, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2', 
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    table = dynamodb.Table('order')
+    response = table.scan(
+        FilterExpression=Attr('username').eq(username)
+    )
+    return response['Items']
+
+# Add new order into database
+def put_order(username, product, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2',
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    table = dynamodb.Table('order')
+    response = table.put_item(
+       Item={
+            'username': username,
+            'product': product
+        }
+    )
+    return response
+
+# Get reviews from 'review' table
+def get_reviews(product, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2', 
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    table = dynamodb.Table('review')
+    response = table.query(
+        KeyConditionExpression=Key('product').eq(product)
+    )
+    return response['Items']
+
+# Create and add new review into database
+def put_review(username, product, subject, message, rating, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2',
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    table = dynamodb.Table('review')
+    response = table.put_item(
+       Item={
+            'username': username,
+            'product': product,
+            'subject': subject,
+            'message': message,
+            'rating': rating
+        }
+    )
+    return response
+
 
 # Lambda Functions =================================================================================
 
@@ -587,6 +758,7 @@ def get_popular():
     items = result['Payload'].read().decode('UTF-8')
     load_items = json.loads(items)
     return load_items
+
 
 # Run App
 if __name__ == "__main__":
